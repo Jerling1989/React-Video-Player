e31dfa1cabf3a541a46ddc0387a535cce0a4a695
Jacob <jerling1989@gmail.com>
2018-05-18 02:40:51 -0400

    youtube api setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyA6KRgUyCWq_OBNmoE3SikZ9R62hp250aA';
+
 // CREATE A NEW COMPONENT
 // THIS COMPONENT SHOULD PRODUCE SOME HTML
 const App = () => {
