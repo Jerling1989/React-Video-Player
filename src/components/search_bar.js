18f0f341287435337f77a60f990b9479f599bcd2
Jacob <jerling1989@gmail.com>
2018-05-18 17:31:23 -0400

    more on state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 
 // CREATE SEARCHBAR CLASS COMPONENT
 class SearchBar extends Component {
+	// INITIALIZE STATE
 	constructor(props) {
 		super(props);
-
+		// SET STATE
 		this.state = { term: ''};
 	}
 
 	// RENDER COMPONENT METHOD
 	render() {
-		// RETURN INPUT WITH EVENT HANDLER ARROW FUNCTION
-		return <input onChange={event => console.log(event.target.value)} />;
+		// RETURN INPUT WITH EVENT HANDLER ARROW FUNCTION TO SET STATE
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
 	}
 
 }
