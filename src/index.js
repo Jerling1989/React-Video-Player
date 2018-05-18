5b9e05db6871d1435367480d1161f194a6691553
Jacob <jerling1989@gmail.com>
2018-05-18 00:19:55 -0400

    ES6 import statements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // CREATE A NEW COMPONENT
 // THIS COMPONENT SHOULD PRODUCE SOME HTML
 const App = function () {
@@ -5,3 +7,4 @@ const App = function () {
 };
 
 // TAKE THIS COMPONENTS GENERATED HTML AND PUT IN THE DOM
+React.render(App);
