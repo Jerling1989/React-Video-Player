3c467752cde358abc620373689de7b3af74aae9b
Jacob <jerling1989@gmail.com>
2018-05-18 16:39:29 -0400

    SearchBar event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,15 @@
+// IMPORT REACT
 import React, { Component } from 'react';
 
+// CREATE SEARCHBAR CLASS COMPONENT
 class SearchBar extends Component {
+	// RENDER COMPONENT METHOD
 	render() {
-		return <input />;
+		// RETURN INPUT WITH EVENT HANDLER ARROW FUNCTION
+		return <input onChange={event => console.log(event.target.value)} />;
 	}
+
 }
 
+// EXPORT COMPONENT
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+// IMPORT REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// IMPORT COMPONENTS
 import SearchBar from './components/search_bar';
-
+// YOUTUBE API KEY
 const API_KEY = 'AIzaSyA6KRgUyCWq_OBNmoE3SikZ9R62hp250aA';
 
 // CREATE A NEW COMPONENT
