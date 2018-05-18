8b6a567fa1614c3aed4c1adec2fbebd4388f66bf
Jacob <jerling1989@gmail.com>
2018-05-18 02:54:18 -0400

    export SearchBar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyA6KRgUyCWq_OBNmoE3SikZ9R62hp250aA';
 
 // CREATE A NEW COMPONENT
 // THIS COMPONENT SHOULD PRODUCE SOME HTML
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+		);
 };
 
 // TAKE THIS COMPONENTS GENERATED HTML AND PUT IN THE DOM
