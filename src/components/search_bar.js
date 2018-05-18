32219e603543699c2cb387ee8e5b670da5f2cb33
Jacob <jerling1989@gmail.com>
2018-05-18 17:18:03 -0400

    intro to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 
 // CREATE SEARCHBAR CLASS COMPONENT
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
+	}
+
 	// RENDER COMPONENT METHOD
 	render() {
 		// RETURN INPUT WITH EVENT HANDLER ARROW FUNCTION
