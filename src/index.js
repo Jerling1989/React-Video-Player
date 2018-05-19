c5211feedbdeec722abce67bfe65e4a81d635bda
Jacob <jerling1989@gmail.com>
2018-05-19 18:02:59 -0400

    youtube search response

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // IMPORT REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 // IMPORT COMPONENTS
 import SearchBar from './components/search_bar';
 // YOUTUBE API KEY
 const API_KEY = 'AIzaSyA6KRgUyCWq_OBNmoE3SikZ9R62hp250aA';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+})
 
 // CREATE A NEW COMPONENT
 // THIS COMPONENT SHOULD PRODUCE SOME HTML
