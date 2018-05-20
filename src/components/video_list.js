b7e99817353c4c101cadb40e40f34964027dad93
Jacob <jerling1989@gmail.com>
2018-05-20 16:16:29 -0400

    building list with map

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,19 @@
+// IMPORT REACT
 import React from 'react';
+// IMPORT COMPONENT
+import VideoListItem from './video_list_item';
 
+// CREATE VIDEOLIST FUNCTIONAL COMPONENT WITH PROPS PASSED AS ARGUMENT
 const VideoList = (props) => {
+	// CREATE VIDEOITEMS ARRAY BY MAPPING THE VIDEOS PROP
+	const videoItems = props.videos.map(video => <VideoListItem video={video} />);
+
 	return (
 		<ul className="col-md-4 list-group">
-			{props.videos.length}
+			{videoItems}
 		</ul>
 	);
 };
 
+// EXPORT COMPONENT
 export default VideoList;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+// IMPORT REACT
+import React from 'react';
+
+// CREATE VIDEOLISTITEM FUNCTIONAL COMPONENT
+const VideoListItem = (props) => {
+	return <li>Video</li>
+};
+
+
+// EXPORT COMPONENT
+export default VideoListItem;
