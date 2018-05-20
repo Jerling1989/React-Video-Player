9e960bc12e262e45de7d74ba13fcd225c6aad9e2
Jacob <jerling1989@gmail.com>
2018-05-20 16:51:05 -0400

    adding thumbnail and title to video list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,6 +6,7 @@ import VideoListItem from './video_list_item';
 // CREATE VIDEOLIST FUNCTIONAL COMPONENT WITH PROPS PASSED AS ARGUMENT
 const VideoList = (props) => {
 	// CREATE VIDEOITEMS ARRAY BY MAPPING THE VIDEOS PROP
+	// CREATE VIDEOLISTITEM COMPONENT FOR EACH ITERATION AND PASS VIDEO PROP
 	const videoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />);
 
 	return (
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,26 @@
 // IMPORT REACT
 import React from 'react';
 
-// CREATE VIDEOLISTITEM FUNCTIONAL COMPONENT
-const VideoListItem = (props) => {
-	return <li>Video</li>
+// CREATE VIDEOLISTITEM FUNCTIONAL COMPONENT WITH PROPS.VIDEO ARGUMENT
+const VideoListItem = ({video}) => {
+	// CREATE CONST FOR VIDEO THUMBNAIL URL
+	const imageUrl = video.snippet.thumbnails.default.url;
+
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">
+						{video.snippet.title}
+					</div>
+				</div>
+			</div>
+		</li>
+	); 
 };
 
 
